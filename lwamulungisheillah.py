e485ded8c2840fabf179aa32a6911a62bae4ccdc
Lwamulungisheillah <143074288+Lwamulungisheillah@users.noreply.github.com>
2023-09-13 17:20:30 +0300

    Add files via upload

---
+++ lwamulungisheillah.py
@@ -0,0 +1,44 @@
+# (i) name_even_numbers_less_than_8 = a
+a = 2,4,6
+total_even_number =(sum(a))
+print(a)
+print(sum(a))
+print("the sum of even numbers less than 8 is"+" "+str(sum(a)))
+
+
+
+# (ii) average_of_any_five_numbers_from_the_keyboard
+numbers = 1,3,5,7,9
+total_number = 5
+sum_of_numbers = (sum(numbers))
+print(sum_of_numbers)
+average=(sum_of_numbers/ total_number)
+print(average)
+
+
+
+# (iii)a)
+a = (-5+8*6)
+print(a)
+
+
+# (iii)b)
+b = ((55+9)%9)
+print(b)
+
+
+# (iii)c)
+c = (20+-3*5/8)
+print(c)
+
+# (iii)d)
+d = (5+15/3*2-8%3)
+print(d)
+
+
+
+#(iv)animals
+animals = ("cows,goats,sheep,pigs")
+animal_list = ["cows","goats","sheep","pigs"]
+print(animal_list)
+
